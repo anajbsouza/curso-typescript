eeeecb8304c23f723d91c0da1808b44f893eaf6a
anajbsouza <anaju.batsouza@gmail.com>
2023-09-13 00:27:39 -0300

    ex 5

---
+++ aula-ts-05/index.ts
@@ -0,0 +1,8 @@
+//tipagens dos arrays
+
+const numbers: number[] = [1,2,3,4,5];
+const anwsers: Array<boolean> = [true, false, false, true, false];
+const strings: string[] = ["one", "two", "three", "four", "five"];
+const mix: Array<number | string> = [1,"two",3,"four",5];
+const mix2: (number | boolean)[] = [5, true, 10, false];
+const floatNumbers: Array<number> = [1.0,2.5,3.0,4.1,5.5];
