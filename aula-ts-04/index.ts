4f6ce0b7fe61a55e8d692b95a3105ee6f81fd62a
anajbsouza <anaju.batsouza@gmail.com>
2023-09-14 22:58:12 -0300

    ex 9

--- aula-ts-04/index.ts
+++ aula-ts-04/index.ts
@@ -1,3 +1,5 @@
+import { id } from "date-fns/locale";
+
 function multiply(num1, num2) {
     return num1 * num2;
 }
@@ -19,14 +21,19 @@ function showResult(result: number) {
 }
 
 (() => {
-    const num1: number = parseFloat(prompt("First Number") || "");
-    const num2: number = parseFloat(prompt("Second Number")|| "");
+    //const num1: number = parseFloat(prompt("First Number") || "");
+    const num1 = Number(prompt("First Number"))
+    // const num2: number = parseFloat(prompt("Second Number")|| "");
+    const num2 = Number(prompt("Second Number"));
 
-    if (!isNaN(num1) && !isNaN(num1) && num1 !== null && num2 !== null){
-        let result = sum(num1,num2);
-        result += multiply(1,2);
-        showResult(result);
+    if(!num1 || !num2) {
+        console.log('Insira os 2 números!');
+    } else {
+        if (!isNaN(num1) && !isNaN(num1)){
+            //&& num1 !== null && num2 !== null
+            let result = sum(num1,num2);
+            result += multiply(1,2);
+            showResult(result);
+        }
     }
-    
-    
 })();
--- aula-ts-09/src/routers/music-router.ts
+++ aula-ts-09/src/routers/music-router.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import { validateSchemaMiddleware } from "middlewares/validateSchema";
+import { musicSchema } from "schemas/musicSchema";
 import musicController from "../controllers/music-controller";
 
 const musicRouter = Router();
 
 musicRouter.get("/musics", musicController.getMusics);
-musicRouter.post("/musics", musicController.createMusic); // TODO: validação via Joi
+musicRouter.post("/musics", validateSchemaMiddleware(musicSchema), musicController.createMusic); // TODO: validação via Joi
 
 export default musicRouter;
