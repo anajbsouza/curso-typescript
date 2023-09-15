3865aba89efc67aeb5e20414a5b301f47d02597f
anajbsouza <anaju.batsouza@gmail.com>
2023-09-14 22:58:34 -0300

    ex 10

--- aula-ts-10/index.ts
+++ aula-ts-10/index.ts
@@ -3,7 +3,3 @@ function identity<T> (value: T) : T {
 }
 
 console.log(identity(1)) // 1
-
-console.log(identity<number>(42)); // 42
-console.log(identity<string>("Hello")); // Hello
-console.log(identity<number[]>([1, 2, 3])); // [1, 2, 3]
